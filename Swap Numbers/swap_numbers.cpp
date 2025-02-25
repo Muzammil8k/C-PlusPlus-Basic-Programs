ef6c2e506b2bedbe799a5314a748de4a1cbd3f5a
MUZAMMIL MALIK <134679759+Muzammil8k@users.noreply.github.com>
2025-02-25 16:32:18 +0500

    Add files via upload

--- Swap Numbers/swap_numbers.cpp
+++ Swap Numbers/swap_numbers.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int num1, num2, temp; // Declare three integer variables
+
+    // Prompt user to enter the first number
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    // Prompt user to enter the second number
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Display numbers before swapping
+    cout << "\nBefore swapping" << endl;
+    cout << "First number: " << num1 << endl;
+    cout << "Second number: " << num2 << endl;
+
+    // Swap the values of num1 and num2 using temp as a temporary storage
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    // Display numbers after swapping
+    cout << "\nAfter swapping" << endl;
+    cout << "First number: " << num1 << endl;
+    cout << "Second number: " << num2 << endl;
+
+    return 0; // Return 0 to indicate successful execution
+}
